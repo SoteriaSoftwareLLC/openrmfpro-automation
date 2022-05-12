11644b579400e6ab38e6b7ae581cdeec8c55978b
DaleBinghamSoteriaSoft <dale.bingham@soteriasoft.com>
2022-05-12 13:00:15 -0400

    Update for v2.8 dashboard and scripts

--- python/authentication.py
+++ python/authentication.py
@@ -1,13 +1,16 @@
+import sys
+# Test the authentication and application key / token combination
+# ex: python3 uploadSystemPackageSCAP.py http://192.168.13.111:8080 hvs.xxxxxxxxxxxxxxxx openrmfprosvc
+
 import requests
 from requests.structures import CaseInsensitiveDict
 
-url = "http://192.168.13.114:8080/api/external/testauthentication"
+url = sys.argv[1] + "/api/external/testauthentication"
 
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/x-www-form-urlencoded"
-headers["Authorization"] = "Bearer s.xxxxxxxxxxxxxxxxxxxxxxx"
-
-data = "applicationKey=degthatuploader"
+headers["Authorization"] = "Bearer " + sys.argv[2]
+data = "applicationKey=" + sys.argv[3]
 
 resp = requests.post(url, headers=headers, data=data)
 
