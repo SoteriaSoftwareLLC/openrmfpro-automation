123b95be3b4d74acd5f845d5249bb56046079440
DaleBinghamSoteriaSoft <dale.bingham@soteriasoft.com>
2022-05-12 16:26:32 -0400

    updated Python scripts to use parameters

--- python/listTemplateChecklist.py
+++ python/listTemplateChecklist.py
@@ -1,16 +1,17 @@
+# list checklist templates by DISA, CIS, Custom or Organizational
+# ex: python3 listTemplateChecklist.py http://192.168.13.111:8080 disa openrmfprosvc  hvs.xxxxxxxxxxxxxxxx Cisco
+
+import sys
 import requests
 from requests.structures import CaseInsensitiveDict
 
-TemplateType = "disa"
-
-url = "http://192.168.13.114:8080/api/external/templates/"+TemplateType+"/?applicationKey=degthatuploader&searchString=Cisco"
+url = sys.argv[1] + "/api/external/templates/" + sys.argv[2]+ "/?applicationKey=" + sys.argv[3] + "&searchString=" + sys.argv[5]
 
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
-headers["Authorization"] = "Bearer s.xxxxxxxxxxxxxxxxxxxxxxx"
+headers["Authorization"] = "Bearer " + sys.argv[4]
 
 resp = requests.get(url, headers=headers)
 
 print(resp.status_code)
-#print(resp.json)
 print(resp.text)
