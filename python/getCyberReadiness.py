93752ac5e564093ca96721e12d0a49782df7c2d9
DaleBinghamSoteriaSoft <dale.bingham@soteriasoft.com>
2023-02-13 14:50:27 -0500

    adding readiness calls

---
+++ python/getCyberReadiness.py
@@ -0,0 +1,19 @@
+# get the Cyber Readiness Scores for a system package
+# ex: python3 getCyberReadiness.py http://192.168.13.111:8080 companyinfra openrmfprosvc hvs.xxxxxxxxxxxxxxx
+
+import sys
+import json
+import requests
+from requests.structures import CaseInsensitiveDict
+
+url = sys.argv[1] + "/api/external/systempackage/" + sys.argv[2]+ "/readinessscore/?applicationKey=" + sys.argv[3]
+
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
+headers["Authorization"] = "Bearer " + sys.argv[4]
+
+resp = requests.get(url, headers=headers)
+
+print(resp.status_code)
+json_object = json.loads(resp.text)
+print(json.dumps(json_object, indent=1))
