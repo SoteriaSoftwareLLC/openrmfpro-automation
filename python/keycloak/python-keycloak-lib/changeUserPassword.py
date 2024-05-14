82cbd94abdd43c5e826fcd0a38e99f9eb1161ea3
MackenzieBingham <mackenzie.bingham@soteriasoft.com>
2024-05-14 15:54:33 -0400

    add additional Keycloak python scripts

--- python/keycloak/python-keycloak-lib/changeUserPassword.py
+++ python/keycloak/python-keycloak-lib/changeUserPassword.py
@@ -1,6 +1,6 @@
 # Get a user record for the parameters passed in, if it is there
 # fix formatting and return JSON
-# to run:  python3 deleteUserByRealmAndUsername.py https://keycloak.mycompany.com/auth/ openrmfpro python-keycloak 8675867tyjhgjghuy5675&JKHLKJH user.name
+# to run:  python3 changeUserPassword.py https://keycloak.mycompany.com/auth/ openrmfpro python-keycloak 8675867tyjhgjghuy5675&JKHLKJH user.name newpassword
 
 import sys
 import json
@@ -17,8 +17,9 @@ keycloak_connection = KeycloakOpenIDConnection(
 keycloak_admin = KeycloakAdmin(connection=keycloak_connection)
 
 user_id_keycloak = keycloak_admin.get_user_id(sys.argv[5])
+
 if user_id_keycloak is None:
     print("User Id was not found for that Username\n")
 else:
-    response = keycloak_admin.delete_user(user_id=user_id_keycloak)
-    print(response)
+    response = keycloak_admin.set_user_password(user_id_keycloak, sys.argv[6], temporary=True)
+    print("User successfully\n")
