1fd1b8dd666db5609ea5939b48a902ff2ffd8744
James Mcdermott <james.mcdermott@soteriasoft.com>
2025-09-26 16:58:54 -0400

    bug fix

--- python/systempackage/addSystemPackage.py
+++ python/systempackage/addSystemPackage.py
@@ -1,31 +1,30 @@
 # Add a brand new system package
 # make sure you have the SystemPackageAdministrator Role
 # API call from Developer's Guide: /api/external/systempackage/?applicationKey={applicationKey}
-# ex: python3 addNewSystemPackageWithFramework.py http://192.168.13.111:8080 openrmfprosvc hvs.xxxxxxxxxxxxxx MyTitle mykeywithlowercaseletters "My description" "My First and Last" 855-763-0848 info@soteriasoft.com true MYACRONYM 68bb00bb05c51ab74f106611c "68bb00b05c51ab74f1066129|68bb00b05c51ab74f106612c|68bb00b05c51ab74f1066131"
+# ex: python3 addSystemPackage.py http://192.168.13.111:8080 openrmfprosvc hvs.xxxxxxxxxxxxxx MyTitle mykeywithlowercaseletters "My description" "My First and Last" 855-763-0848 info@soteriasoft.com true MYACRONYM 68bb00bb05c51ab74f106611c "68bb00b05c51ab74f1066129|68bb00b05c51ab74f106612c|68bb00b05c51ab74f1066131"
 
 import sys
 import requests
 from requests.structures import CaseInsensitiveDict
 
-# The minimum expected arguments count is 20 (3 credentials + 17 data fields)
 MIN_ARGS = 13
 
 if len(sys.argv) < MIN_ARGS:
-    print("Usage: python3 addNewSystemPackageWithFramework.py <URL> <applicationKey> <token> <title> <systemKey> <description> <pocName> <pocPhone> <pocEmail> <addUserToSystemPackage> <acronym> <frameworkId> <frameworkLevelList>")
+    print("Usage: python3 addSystemPackage.py <URL> <applicationKey> <token> <title> <systemKey> <description> <pocName> <pocPhone> <pocEmail> <addUserToSystemPackage> <acronym> <frameworkId> <frameworkLevelList>")
     sys.exit(1)
 
 base_url = sys.argv[1]
 app_key = sys.argv[2]
 token = sys.argv[3]
-title = sys.argv[4],
-system_key = sys.argv[5],
-description = sys.argv[6],
-poc_name = sys.argv[7],
-poc_phone = sys.argv[8],
-poc_email = sys.argv[9],
-add_user_to_system_package = sys.argv[10],
-acronym = sys.argv[11],
-framework_id = sys.argv[12],
+title = sys.argv[4]
+system_key = sys.argv[5]
+description = sys.argv[6]
+poc_name = sys.argv[7]
+poc_phone = sys.argv[8]
+poc_email = sys.argv[9]
+add_user_to_system_package = sys.argv[10]
+acronym = sys.argv[11]
+framework_id = sys.argv[12]
 framework_level_list = sys.argv[13]
 
 data = {
@@ -46,7 +45,7 @@ url = f"{base_url}{endpoint}?applicationKey={app_key}"
 
 headers = CaseInsensitiveDict()
 headers["Authorization"] = f"Bearer {token}"
-headers["Content-Type"] = "multipart/form-data"
+headers["Content-Type"] = "application/x-www-form-urlencoded"
 
 try:
     resp = requests.post(url, headers=headers, data=data)
